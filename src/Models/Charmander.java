5d20269a06dd05a3e9e01e5df5a279f6532c3da2
Mikael Eureka <mikael.eureka@gmail.com>
2026-04-04 21:46:18 +0700

    Membuat class Abstact item untuk Item Heal dan testing Heal item

--- src/Models/Charmander.java
+++
@@ -1,9 +0,0 @@
-package Models;
-
-import utils.Type;
-
-public class Charmander extends FirePokemon {
-    public Charmander(String name, int hp, int attack, int defense) {
-        super(name, hp, attack, defense, Type.FIRE);
-    }
-}
--- src/Models/Pikachu.java
+++
@@ -1,9 +0,0 @@
-package Models;
-
-import utils.Type;
-
-public class Pikachu extends ElectricPokemon {
-    public Pikachu(String name, int hp, int attack, int defense) {
-        super(name, hp, attack, defense, Type.ELECTRIC);
-    }
-}
